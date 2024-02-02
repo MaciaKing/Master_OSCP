50897bd8163bff8c41ebcd093e0e58128a98f252
MaciaKing <maciasalvasalva@gmail.com>
2024-02-02 17:59:58 +0100

    [feat]: creating basic structure for script_scapy.py Now the script ask you for a rhost. Also if do you want, you can define a port to test it

---
+++ Master/M4/script_scapy.py
@@ -0,0 +1,16 @@
+from scapy import *
+import argparse
+import pdb
+
+def init_params():
+    msg = "This script is an ARP ping scanner."
+    parser = argparse.ArgumentParser(description = msg)
+    parser.add_argument('-rh','--rhost', type = str, required = True, help='Ip of the rhost.')
+    parser.add_argument('-p','--port', type = str, help='Define specific port. By default is 1234.')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = init_params()
+    rhost = args.rhost
+    #print(rhost)
+
